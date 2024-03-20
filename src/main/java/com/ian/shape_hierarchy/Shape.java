35d078c40b6d45bc065298290dd0329cf26d3abe
iankristoper <iankristoper26@gmail.com>
2024-03-20 22:21:31 +0800

    UPDATE: shape class features

---
+++ src/main/java/com/ian/shape_hierarchy/Shape.java
@@ -0,0 +1,14 @@
+package com.ian.shape_hierarchy;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+//abstract shape -> means   
+public abstract class Shape 
+{
+    public abstract double getArea();
+    public abstract double getPerimeter();
+    
+}
