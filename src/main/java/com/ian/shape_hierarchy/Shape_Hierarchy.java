5e2e8f0105f380c8583dc514a346cd564a712b6f
iankristoper <iankristoper26@gmail.com>
2024-03-20 18:45:40 +0800

    UPDATE: main class features

---
+++ src/main/java/com/ian/shape_hierarchy/Shape_Hierarchy.java
@@ -0,0 +1,16 @@
+package com.ian.shape_hierarchy;
+
+
+
+/**
+ *
+ * @author Ian Tagano
+ */
+public class Shape_Hierarchy 
+{
+
+    public static void main(String[] args) 
+    {
+        
+    }
+}
