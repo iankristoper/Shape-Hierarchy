693accb4d5c4b63df32058fd92d1b924c9f99243
iankristoper <iankristoper26@gmail.com>
2024-03-23 09:09:30 +0800

    UPDATE: shape class features

--- src/main/java/com/ian/shape_hierarchy/Shape.java
+++ src/main/java/com/ian/shape_hierarchy/Shape.java
@@ -5,10 +5,11 @@ package com.ian.shape_hierarchy;
  * @author Ian Tagano
  */
 
-//abstract shape -> means   
+//abstract class shape -> means this is the base class for subclasses 
+//This is the base class 
 public abstract class Shape 
 {
+    //abstract methods also use for subclasses
     public abstract double getArea();
     public abstract double getPerimeter();
-    
 }
