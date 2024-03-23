17e32b1ed90b74e309e8ad4d25f1ab7ddd147552
iankristoper <iankristoper26@gmail.com>
2024-03-23 09:07:45 +0800

    UPDATE: main class features

--- src/main/java/com/ian/shape_hierarchy/Shape_Hierarchy.java
+++ src/main/java/com/ian/shape_hierarchy/Shape_Hierarchy.java
@@ -1,16 +1,32 @@
 package com.ian.shape_hierarchy;
 
 
-
 /**
  *
  * @author Ian Tagano
  */
+
+
 public class Shape_Hierarchy 
 {
 
     public static void main(String[] args) 
     {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4,6);
+        Triangle triangle = new Triangle(4,5,6);
         
+        System.out.println("Circle: ");
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Perimeter: " + circle.getPerimeter());
+        
+        System.out.println("Rectangle: ");
+        System.out.println("Area: " + rectangle.getArea());
+        System.out.println("Perimeter: " + rectangle.getPerimeter());
+        
+        System.out.println("Triangle: ");
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+         
     }
 }
